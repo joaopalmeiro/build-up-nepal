e9d6d964735bf278cb86cd7562b8cd4b0047c81c
João Palmeiro <joaommpalmeiro@gmail.com>
2022-01-25 16:05:39 +0000

    Add Nepal GeoJSON

--- data/README.md
+++ data/README.md
@@ -4,7 +4,7 @@
 
 - `pipenv install --python 3.8`.
 - `pipenv run jupyter lab`.
-- `pipenv run black prep.ipynb`.
+- `pipenv run black prep.ipynb` and/or `pipenv run black map.ipynb`.
 
 ## Notes
 
--- src/NepalMap.jsx
+++ src/NepalMap.jsx
@@ -1,12 +1,10 @@
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
 
-const geoUrl = 'https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json';
-
 // https://www.react-simple-maps.io/docs/getting-started/
 function NepalMap() {
     return (
-        <ComposableMap projection="geoAlbersUsa">
-            <Geographies geography={geoUrl}>
+        <ComposableMap>
+            <Geographies geography={mapData}>
                 {({ geographies }) =>
                     geographies.map((geo) => <Geography key={geo.rsmKey} geography={geo} />)
                 }
