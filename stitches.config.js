d76aa0a5c037120b2811bca9298ec0dc794c55c5
João Palmeiro <joaommpalmeiro@gmail.com>
2022-01-27 09:30:58 +0000

    Update stitches.config.js

--- stitches.config.js
+++ stitches.config.js
@@ -11,6 +11,40 @@ import { createStitches } from '@stitches/react';
 export const { styled, css, globalCss, keyframes, getCssText, theme, createTheme, config } =
     createStitches({
         theme: {
+            fonts: {
+                // https://github.com/mantinedev/mantine/blob/3.6.3/src/mantine-styles/src/theme/default-theme.ts#L18
+                sans: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji'
+            },
+            fontSizes: {
+                1: '12px',
+                2: '13px',
+                3: '15px',
+                4: '17px',
+                5: '19px',
+                6: '21px',
+                7: '27px',
+                8: '35px',
+                9: '59px'
+            },
+            radii: {
+                1: '4px',
+                2: '6px',
+                3: '8px',
+                4: '12px',
+                pill: '9999px',
+                round: '50%'
+            },
+            sizes: {
+                1: '5px',
+                2: '10px',
+                3: '15px',
+                4: '20px',
+                5: '25px',
+                6: '35px',
+                7: '45px',
+                8: '65px',
+                9: '80px'
+            },
             space: {
                 1: '5px',
                 2: '10px',
