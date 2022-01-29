5cde4995ac5308a6b40811c8ac7b6ef682f14379
João Palmeiro <joaommpalmeiro@gmail.com>
2022-01-29 16:52:26 +0000

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import Heading from './Heading';
 
 // https://emojipedia.org/flag-nepal/
 function Header() {
-    return <Heading size="4">Build it up, Nepal!</Heading>;
+    return <Heading size="4">build it up, Nepal!</Heading>;
 }
 
 export default Header;
