95da740e9fe8ef729cb5b0f210417cd7813a5950
João Palmeiro <joaommpalmeiro@gmail.com>
2022-01-31 10:00:22 +0000

    Closes #33

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,6 +5,7 @@ import Paragraph from './Paragraph';
 // https://emojipedia.org/flag-nepal/
 // https://emojipedia.org/brick/
 // https://github.com/radix-ui/design-system/blob/v0.6.2/pages/index.tsx#L1505
+// https://github.com/radix-ui/design-system/blob/v0.6.2/pages/index.tsx#L917
 function Header() {
     return (
         <>
@@ -13,6 +14,11 @@ function Header() {
                 <Link href="https://www.buildupnepal.com/" target="_blank" rel="noreferrer">
                     Build up Nepal
                 </Link>
+            </Paragraph>
+            <Paragraph css={{ alignSelf: 'start' }}>
+                The start of these projects and enterprises goes from 2015 to April 2021 — thus,
+                take into account that the data shown here corresponds to a snapshot at a given
+                moment in time.
             </Paragraph>
         </>
     );
