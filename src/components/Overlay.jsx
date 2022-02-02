7b8b8daf3221a490f0e17cb9a0e82634598ac50d
João Palmeiro <joaommpalmeiro@gmail.com>
2022-02-02 19:39:08 +0000

    Update Overlay and Panel

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,8 +1,8 @@
-import { styled } from '../../stitches.config';
+import { css, styled } from '../../stitches.config';
 
 // Source: https://github.com/radix-ui/design-system/blob/v0.6.2/components/Overlay.tsx
-const Overlay = styled('div', {
+export const overlayStyles = css({
     backgroundColor: 'rgba(0, 0, 0, 0.15)'
 });
 
-export default Overlay;
+export const Overlay = styled('div', overlayStyles);
--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,10 +1,10 @@
-import { styled } from '../../stitches.config';
+import { css, styled } from '../../stitches.config';
 
 // Source: https://github.com/radix-ui/design-system/blob/v0.6.2/components/Panel.tsx
-const Panel = styled('div', {
+export const panelStyles = css({
     backgroundColor: '$panel',
     borderRadius: '$3',
     boxShadow: '$colors$shadowLight 0px 10px 38px -10px, $colors$shadowDark 0px 10px 20px -15px'
 });
 
-export default Panel;
+export const Panel = styled('div', panelStyles);
