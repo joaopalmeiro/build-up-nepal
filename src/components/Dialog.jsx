29b09b9406a45fc9041ad4eb0a422f1d0cc902c9
João Palmeiro <joaommpalmeiro@gmail.com>
2022-02-02 19:14:07 +0000

    Update Dialog.jsx

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -5,7 +5,7 @@
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { Cross1Icon } from '@radix-ui/react-icons';
 
-import { styled, CSS } from '../../stitches.config';
+import { styled } from '../../stitches.config';
 
 import { IconButton } from './IconButton';
 import { overlayStyles } from './Overlay';
@@ -17,6 +17,24 @@ const StyledOverlay = styled(DialogPrimitive.Overlay, overlayStyles, {
     right: 0,
     bottom: 0,
     left: 0
+});
+
+const StyledContent = styled(DialogPrimitive.Content, panelStyles, {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+
+    minWidth: 200,
+    maxHeight: '85vh',
+    padding: '$4',
+    marginTop: '-5vh',
+
+    transform: 'translate(-50%, -50%)',
+    willChange: 'transform',
+
+    '&:focus': {
+        outline: 'none'
+    }
 });
 
 // TODO
